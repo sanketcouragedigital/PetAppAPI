e7d5f504c11ef643604956f5b2d0829611c607c2
sanketcouragedigital <sanketdhotre@couragedigital.com>
2016-02-15 18:00:39 +0530

    My listing delete image

--- dao/MyListingDAO.php
+++ dao/MyListingDAO.php
@@ -84,13 +84,30 @@ class MyListingDAO
     }
 	public function deleteMyListingPetList($MyListingPetList) {
 		 try {
-			 $sql = "DELETE FROM petapp WHERE id='".$MyListingPetList->getId()."' AND email='".$MyListingPetList->getEmail()."' ";
-			 $isDeleted = mysqli_query($this->con, $sql);
-                if ($isDeleted) {
-					$this->data = "MyListing_Pet_Deleted";
-                } else {
-                    $this->data = "ERROR";
+            $sql = "SELECT * FROM petapp WHERE id = '".$MyListingPetList->getId()."' AND email = '".$MyListingPetList->getEmail()."'";
+            $fetchData = mysqli_query($this->con, $sql);
+            $this->result=array();
+            while ($rowdata = mysqli_fetch_assoc($fetchData)) {
+                $this->result[]=$rowdata;
+            }
+            $firstImagePath = $this->result[0]['first_image_path'];
+            $secondImagePath = $this->result[0]['second_image_path'];
+            $thirdImagePath = $this->result[0]['third_image_path'];
+            
+			$sql = "DELETE FROM petapp WHERE id='".$MyListingPetList->getId()."' AND email='".$MyListingPetList->getEmail()."' ";
+			$isDeleted = mysqli_query($this->con, $sql);
+            if ($isDeleted) {
+                $this->data = "MyListing_Pet_Deleted";
+                $petImagePaths = array($firstImagePath, $secondImagePath, $thirdImagePath);
+                foreach ($petImagePaths as $index => $petImagePath) {
+                    if($petImagePath != "" && !empty($petImagePath)) {
+                        chmod($petImagePath, 777);
+                        unlink($petImagePath);
+                    }
                 }
+            } else {
+                $this->data = "ERROR";
+            }
 			 
 		} catch(Exception $e) {
             echo 'SQL Exception: ' .$e->getMessage();
@@ -100,13 +117,30 @@ class MyListingDAO
 	
 	public function deleteMyListingPetMateList($MyListingPetmateList) {
 		 try {
-			  $sql = "DELETE FROM petmate WHERE id='".$MyListingPetmateList->getId()."' AND email='".$MyListingPetmateList->getEmail()."' ";
-			  $isDeleted = mysqli_query($this->con, $sql);
-                if ($isDeleted) {
-					$this->data = "MyListing_PetMate_Deleted";
-                } else {
-                    $this->data = "ERROR";
+		    $sql = "SELECT * FROM petmate WHERE id = '".$MyListingPetList->getId()."' AND email = '".$MyListingPetList->getEmail()."'";
+            $fetchData = mysqli_query($this->con, $sql);
+            $this->result=array();
+            while ($rowdata = mysqli_fetch_assoc($fetchData)) {
+                $this->result[]=$rowdata;
+            }
+            $firstImagePath = $this->result[0]['first_image_path'];
+            $secondImagePath = $this->result[0]['second_image_path'];
+            $thirdImagePath = $this->result[0]['third_image_path'];
+            
+			$sql = "DELETE FROM petmate WHERE id='".$MyListingPetmateList->getId()."' AND email='".$MyListingPetmateList->getEmail()."' ";
+			$isDeleted = mysqli_query($this->con, $sql);
+            if ($isDeleted) {
+                $this->data = "MyListing_PetMate_Deleted";
+                $petImagePaths = array($firstImagePath, $secondImagePath, $thirdImagePath);
+                foreach ($petImagePaths as $index => $petImagePath) {
+                    if($petImagePath != "" && !empty($petImagePath)) {
+                        chmod($petImagePath, 777);
+                        unlink($petImagePath);
+                    }
                 }
+            } else {
+                $this->data = "ERROR";
+            }
 		} catch(Exception $e) {
             echo 'SQL Exception: ' .$e->getMessage();
         }
